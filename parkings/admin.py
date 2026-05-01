583340ad853178b79dfaf8c1e62c8e8ab0d65a00
Sandra Milosevic <smilosevic892@gmail.com>
2026-05-01 19:47:54 +0200

    feat: register models in admin panel

--- parkings/admin.py
+++ parkings/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from parkings.models import Parking
 
-# Register your models here.
+admin.site.register(Parking)
--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from reservations.models import Reservation
 
-# Register your models here.
+admin.site.register(Reservation)
